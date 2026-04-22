3facc6d2078c5e465624803e2010c4d804b2cab4
Mouhamad Ahmad <mouhamad.a@bahwancybertek.com>
2026-04-22 19:20:30 +0300

    updating url

--- src/main.jsx
+++ src/main.jsx
@@ -9,26 +9,26 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
-// window.Echo = new Echo({
-//   broadcaster: 'reverb',
-//   key: import.meta.env.VITE_REVERB_APP_KEY,
-//   wsHost: import.meta.env.VITE_REVERB_HOST,
-//   wsPort: 80,
-//   wssPort: 443,
-//   forceTLS: true,
-//   enabledTransports: ['ws', 'wss'],
-// });
-
-
 window.Echo = new Echo({
   broadcaster: 'reverb',
   key: import.meta.env.VITE_REVERB_APP_KEY,
   wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-  wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
+  wsPort: 80,
+  wssPort: 443,
+  forceTLS: true,
   enabledTransports: ['ws', 'wss'],
 });
+
+
+// window.Echo = new Echo({
+//   broadcaster: 'reverb',
+//   key: import.meta.env.VITE_REVERB_APP_KEY,
+//   wsHost: import.meta.env.VITE_REVERB_HOST,
+//   wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//   wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//   forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
+//   enabledTransports: ['ws', 'wss'],
+// });
 
 createRoot(document.getElementById("trocent-id")).render(
   <ContextProvider>
